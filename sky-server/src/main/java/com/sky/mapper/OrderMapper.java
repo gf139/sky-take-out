662a4d5dcf613e20c574f09f866d3a105f188160
GoldenFox <1255516186@qq.com>
2024-08-04 17:56:04 +0800

    订单支付功能代码开发(跳过支付页面)

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -4,6 +4,7 @@ import com.sky.entity.AddressBook;
 import com.sky.entity.Orders;
 import org.apache.ibatis.annotations.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Mapper
@@ -14,4 +15,36 @@ public interface OrderMapper {
      * @param orders
      */
     void insert(Orders orders);
+
+    /**
+     * 根据订单号查询订单
+     * @param orderNumber
+     */
+    @Select("select * from orders where number = #{orderNumber}")
+    Orders getByNumber(String orderNumber);
+
+    /**
+     * 修改订单信息
+     * @param orders
+     */
+    void update(Orders orders);
+
+
+    /**
+     * 获取orderid的mapper层方法，写在OrderMapper.java文件下
+     * @param orderNumber
+     * @return
+     */
+    @Select("select id from orders where number=#{orderNumber}")
+    Long getorderId (String orderNumber);
+
+
+    /**
+     * 用于替换微信支付更新数据库状态的问题
+     * @param orderStatus
+     * @param orderPaidStatus
+     */
+    @Update("update orders set status = #{orderStatus},pay_status = #{orderPaidStatus} ,checkout_time = #{check_out_time} where id = #{id}")
+    void updateStatus(Integer orderStatus, Integer orderPaidStatus,LocalDateTime check_out_time, Long id);
+
 }
--- sky-server/src/main/java/com/sky/mapper/UserMapper.java
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -23,4 +23,6 @@ public interface UserMapper {
      */
     void insert(User user);
 
+    @Select("select * from user where id = #{userId}")
+    User getById(Long userId);
 }
